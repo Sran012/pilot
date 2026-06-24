a8b9d48c131d5d855ad900ac63a0c5a2d86c8a7b
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-06-24 17:59:17 +0000

    feat : to not exit on ctrl+x and render app on 60fps

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -15,5 +15,8 @@ function App() {
   );
 }
 
-const renderer = await createCliRenderer();
+const renderer = await createCliRenderer({
+  targetFps: 30,
+  exitOnCtrlC: false,
+});
 createRoot(renderer).render(<App />);
