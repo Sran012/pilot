bf8f7f33eedc51aa4609b9c96b5e0098b407e010
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-06-24 17:58:05 +0000

    feat : command-menu rendering with all req. function implementation passed in props of command-menu and using usecommandmenu custom hook

--- packages/cli/src/components/input-bar.tsx
+++ packages/cli/src/components/input-bar.tsx
@@ -1,5 +1,13 @@
+import { useEffect,useRef,useCallback } from "react";
+import type { TextareaRenderable } from "@opentui/core";
+import { useRenderer } from "@opentui/react";
+import { CommandMenu } from "./command-menu";
 import { StatusBar } from "./status-bar";
 import type { KeyBinding } from "@opentui/core";
+
+import type { Command } from "./command-menu/types";
+import { useCommandMenu } from "./command-menu/use-command-menu";
+
 type InputBarProps = {
     onSubmit: (text:string)=>void,
     disabled?: boolean
@@ -13,9 +21,85 @@ export const TEXTAREA_KEYBINDINGS:KeyBinding[] = [
 ]
 
 export function Inputbar({onSubmit, disabled}:InputBarProps){
+    const textareaRef = useRef<TextareaRenderable>(null);
+    const onSubmitRef = useRef<() => void>(() => {});
+    const renderer = useRenderer();
+
+    const {
+        ShowCommandMenu,
+        commandQuery,
+        SelectedIndex,
+        scrollRef,
+        handleContentChange,
+        resolveCommand,
+        setSelectedIndex
+    } = useCommandMenu();
+
+    const handleCommandExecute = useCallback((index:number)=> {
+        const command = resolveCommand(index);
+        handleCommand(command);
+    },[])
+
+    const handleTextareaContentChange = useCallback(()=> {
+        const textarea = textareaRef.current;
+        if (!textarea) return;
+
+        handleContentChange(textarea.plainText);
+    },[])
+
+    const handleCommand = useCallback((command:Command |undefined)=> {
+        const textarea = textareaRef.current;
+        if(!textarea || !command) return;
+
+        textarea.setText("");
+
+        if(command.action){
+            command.action({
+                exit: ()=> renderer.destroy(),
+            })    
+        }
+        else {
+            textarea.insertText(command.value + " ");
+        }
+    }, []);
+
+    const handleSubmit = useCallback(()=>{
+        if (disabled) return;
+
+        const textarea = textareaRef.current;
+        if(!textarea) return;
+
+        const text = textarea.plainText.trim();
+        if(text.length === 0) return;
+
+        onSubmit(text);
+        textarea.setText("");
+    }, []);
+
+    useEffect(()=> {
+        const textarea = textareaRef.current;
+        if(!textarea) return;
+
+        textarea.onSubmit = ()=> {
+            onSubmitRef.current();
+        }
+    }, []);
+
+    onSubmitRef.current = ()=>{
+        if(disabled) return;
+
+        if(ShowCommandMenu){
+            const command = resolveCommand(SelectedIndex);
+            handleCommand(command);
+            return;
+        }
+
+        handleSubmit();
+    }
+
     return (
         <box width='100%' alignItems="center">
-            <box borderColor="cyan" border={["left"]}>
+            <box width="100%" borderColor="cyan" border={["left"]}>
                 <box 
                   position="relative"
                   justifyContent="center"
@@ -24,7 +108,17 @@ export function Inputbar({onSubmit, disabled}:InputBarProps){
                   paddingY={1}
                   gap={1}
                   width="100%">
+                    {ShowCommandMenu && (<box position="absolute" width='100%' bottom="100%" left={0} backgroundColor="#1A1A24" zIndex={10}>
+                        <CommandMenu query={commandQuery}
+                        selectedIndex={SelectedIndex}
+                        scrollRef={scrollRef}
+                        onSelect={setSelectedIndex}
+                        onExexute={handleCommandExecute}/>
+                    </box>)}
                     <textarea focused={!disabled} 
+                    ref={textareaRef }
+                    width="100%"
+                    onContentChange={handleTextareaContentChange}
                     keyBindings={TEXTAREA_KEYBINDINGS}
                       placeholder={"Ask me anything... find velnereblities in session management"}
                     />
