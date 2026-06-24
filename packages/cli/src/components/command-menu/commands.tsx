6514e37b8f20124547918da57ab85f2dd5f33009
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-06-24 18:01:29 +0000

    chore : basic commands (temporary)

--- packages/cli/src/components/command-menu/commands.tsx
+++ packages/cli/src/components/command-menu/commands.tsx
@@ -1,15 +1,35 @@
 import type { Command } from "./types";
 
-export const COMMANDS : Command[] = [
+export const COMMANDS: Command[] = [
     {
-        name : "new",
-        description : "start a new conversation",
-        value : '/new'
+        name: "new",
+        description: "start a new conversation",
+        value: '/new'
     },
     {
-        name : "exit",
-        description : "exit from session",
-        value : '/exit',
-        action : (ctx) => ctx.exit()
-    }
+        name: "login",
+        description: "Sign in with your browser",
+        value: "/login",
+    },
+    {
+        name: "logout",
+        description: "Sign out of your account",
+        value: "/logout",
+    },
+    {
+        name: "upgrade",
+        description: "Buy more credits",
+        value: "/upgrade",
+    },
+    {
+        name: "usage",
+        description: "Open billing portal in your browser",
+        value: "/usage",
+    },
+    {
+        name: "exit",
+        description: "exit from session",
+        value: '/exit',
+        action: (ctx) => ctx.exit()
+    },
 ]
